9277688af22afff9bd011c28ac495ee0e57ffa2e
botto54 <bryanlbottorff@gmail.com>
2020-12-19 06:08:59 -0500

    add has many transactions to accounts model, add seed data, migrate seed data, test localhost routes to see data properly and it all works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@min_value = 0.01
+@max_value = 999.99 
+
+Account.create([{
+    account_name: "Bryan's Chase Bank",
+    balance: 10000.00,
+    account_type: "Checking Account"
+    },
+    {
+    account_name: "Chase Amazon Card",
+    balance: 500.00,
+    account_type: "Credit Card Account"
+    }
+])
+
+10.times do 
+    Transaction.create({
+        t_name: ["Target", "Best Buy", "Amazon", "Whole Foods", "Trader Joe's", "7 Eleven", "Shell"].sample,
+        description: ["Gas", "Groceries", "Electronics", "Drinks", "Household Supplies"].sample,
+        value: rand(@min_value..@max_value).round(2),
+        account: Account.all.sample
+    })
+end
